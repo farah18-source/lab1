e0d253019d61aa29ec28f1ef702baf613cbdafd4
farah18-source <112944399+farah18-source@users.noreply.github.com>
2022-09-06 12:59:04 +0200

    Add files via upload

---
+++ CodeSample.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    	cout << "Hello, This is a Demo for Git and GitHub \n";
+   	int id;
+	cout<< "Enter Your ID: " << endl;
+    	cin >> id;
+	cout << endl;
+	return 0;
+}
+
